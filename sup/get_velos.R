97670170a3f1354854038d2d6e2c1d76493ee646
Kyle Wasserberger <wasserberger.kyle@gmail.com>
2021-04-13 11:25:31 -0700

    refine velo extractor

--- sup/get_velos.R
+++ sup/get_velos.R
@@ -6,25 +6,25 @@
 # 
 # Kyle Wasserberger
 #
-# Last Updated: 2021-04-06
+# Last Updated: 2021-04-13
 # ----------------------------------------------------
 
 library(googlesheets4)
 library(tidyverse)
 
-url <- 'https://docs.google.com/spreadsheets/d/1PRBYjZcG9b8jB1vttNicup2MhdabkgZY-tJQMLTPxKc/'
+get_velos <- function(){
 
-# input sheet you'd like to pull
-sheet <- 'p04'
+  sheet <- readline('sheet name: ')
 
-path <- getwd()
+  url <- 'https://docs.google.com/spreadsheets/d/1PRBYjZcG9b8jB1vttNicup2MhdabkgZY-tJQMLTPxKc/'
+  path <- getwd()
 
-data <-  read_sheet(ss = url, sheet = sheet)
+  data <-  read_sheet(ss = url, sheet = sheet)
   data_warmup <- data %>%
     select(c('Throw', 'Vel_warm')) %>%
     drop_na()
   data_warmup$pID <- sheet
-
+  
   data_conds <- data %>%
     select(c('Effort', 'Cond1', 'Cond2', 'Vel_cons')) %>%
     drop_na()
@@ -32,6 +32,19 @@ data <-  read_sheet(ss = url, sheet = sheet)
   data_conds$Effort <- factor(data_conds$Effort, levels = c('50', '75', '100'))
   data_conds$Cond1 <- factor(data_conds$Cond1, levels = c('step', 'crow'))
   data_conds$Cond2 <- factor(data_conds$Cond2, levels = c('rpe','velo'))
+  
+  file_name_warmup <- str_glue(path, '/velo_csvs/', sheet,'_','velos_warmup')
+  file_name_conds <- str_glue(path,'/velo_csvs/',sheet, '_', 'velos_conds')
+  
+  write_csv(data_warmup, file_name_warmup)
+  write_csv(data_conds, file_name_conds)
+  
+  if(file.exists(file_name_conds) == TRUE & file.exists(file_name_warmup) == TRUE){
+    print(str_glue('velos extracted for ', sheet))
+    }else{
+    print('something went wrong, bro')
+      }
+}
 
-write_csv(data_warmup,str_glue(path, '/velo_csvs/', sheet,'_','velos_warmup'))
-write_csv(data_conds, str_glue(path,'/velo_csvs/',sheet, '_', 'velos_conds'))
+get_velos()
+
