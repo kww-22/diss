3f1312dce4ec3051dc9c6fd17578447dcfa8f880
Kyle Wasserberger <wasserberger.kyle@gmail.com>
2021-04-20 08:38:53 -0700

    remove interactive prompt in velo_long_maker

--- sup/velo_long_maker.R
+++ sup/velo_long_maker.R
@@ -2,23 +2,22 @@
 # velo_long_maker.R
 #
 # Purpose: to concatenate participant velo csvs into
-# into one long-format mater csv
+# one long-format mater csv
 #
 # author: Kyle Wasserberger
 #
 # Last Updated: 2021-04-15
 # ----------------------------------------------------
 
+library(tidyverse)
+
 # get directory path and file info
 path <- str_glue(getwd(),'sup/velo_csvs',.sep = '/')
 files <- list.files(path = path, pattern = '_')
 
-# interactive input for number of participants
-num_peeps <- 6
-
 # create numeric vectors so for loops can separate warmup and condition velo files
-file_num_conds <- seq(1,num_peeps*2,2)
-file_num_warm <- seq(2,num_peeps*2,2)
+file_num_conds <- seq(1,length(files),2)
+file_num_warm <- seq(2,length(files),2)
 
 # initialize blank data frames
 data_conds <- data.frame()
@@ -26,14 +25,14 @@ data_warm <- data.frame()
 
 
 # iterate over data frames using row bind
-for(i in 1:num_peeps){
+for(i in 1:I(length(files)/2)){
 
 
   data_new <- read.csv(str_glue(path,files[file_num_conds[i]], .sep = '/'))
   data_conds <- rbind.data.frame(data_conds, data_new)
 }
 
-for(i in 1:num_peeps){
+for(i in 1:I(length(files)/2)){
   
   
   data_new <- read.csv(str_glue(path,files[file_num_warm[i]], .sep = '/'))
