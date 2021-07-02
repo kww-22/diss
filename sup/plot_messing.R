61bd70ee53074231a31dfbbd898339cafd2ce5d8
Kyle Wasserberger <wasserberger.kyle@gmail.com>
2021-07-02 07:46:21 -0700

    adjust plot_messing

--- sup/plot_messing.R
+++ sup/plot_messing.R
@@ -2,37 +2,30 @@ library(tidyverse)
 library(patchwork)
 path <- getwd()
 
-d <- read.csv("sup/velo_csvs/masters/data_warm.csv", stringsAsFactors = T)
+please_work <- read.csv("~/GitHub/diss/data/please_work.csv", stringsAsFactors = T)
 
-ggplot(data = d, aes(x = throw, y = velo, color = pID)) +
-  geom_path(size = 1, show.legend = F) +
-  geom_point(size = 2, show.legend = F) +
-  theme_bw() +
-  theme(aspect.ratio = 1,
-        axis.text = element_text(size = 16),
-        axis.title = element_text(size = 16))
-
-
-p1 <- ggplot(data = please_work, aes(x = max_shldr_ir, y = velo, color = pID, label = paste0(pID,".",throw))) +
-  geom_point(size = 1, show.legend = F) +
+p1 <- ggplot(data = subset(please_work, pID %in% please_work$pID[runif(3,1,nrow(please_work))]), aes(x = shldr_ir, y = velo, shape = pID, color = pID, label = paste0(pID,".",throw))) +
+  geom_point(size = 3, show.legend = F) +
   # geom_label() +
   theme_bw() +
   theme(aspect.ratio = 1,
         axis.text = element_text(size = 16),
         axis.title = element_text(size = 14)) +
   xlab("Peak Shoulder ER Torque (Nm)") +
-  ylab("Velo (mph)")
+  ylab("Velo (mph)") +
+  xlim(c(10,95))
 
 
-p2 <- ggplot(data = please_work, aes(x = max_elb_var, y = velo, color = pID, label = paste0(pID,".",throw))) +
-  geom_point(size = 1, show.legend = F) +
+p2 <- ggplot(data = subset(please_work, pID %in% please_work$pID[runif(3,1,nrow(please_work))]), aes(x = elb_var, y = velo, shape = pID, color = pID, label = paste0(pID,".",throw))) +
+  geom_point(size = 3, show.legend =T) +
   # geom_label() +
   theme_bw() +
   theme(aspect.ratio = 1,
         axis.text = element_text(size = 16),
         axis.title = element_text(size = 14)) +
   xlab("Peak Elbow Valgus Torque (Nm)") +
-  ylab("")
+  ylab("") +
+  xlim(c(10,95))
 
 patchwork <- p1 + p2
 
@@ -41,10 +34,3 @@ patchwork + plot_annotation(
   caption = "Kyle Wasserberger (@kwwAU)"
 )
 
-
-test <- subset(please_work, pID == "p15")
-
-plot(test$max_shldr_ir, test$velo)
-
-
-write.csv(please_work, "please_work.csv")
